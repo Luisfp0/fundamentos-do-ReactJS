15b4d5f30b6add876054aadd0075169a16f25631
Luisfp0 <luisfernandodepaulosousa0020@gmail.com>
2023-04-06 10:33:48 -0300

    Formatando o button e adicionando os cards

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,28 @@
 import './style.css';
 
+import {Card} from '../../Components/Card';
+
 export function Home() {
+  function handleNameChange(name) {
+    console.log(name);
+  }
   return (
     <div className='container'>
-      <h1>lista de presença</h1>
-      <input type="text" placeholder='Digite o nome...'/>
-      <button type="button">Adicionar</button>
+      <h1>Lista de presença</h1>
+
+      <input
+       type="text" 
+       placeholder='Digite o nome...'
+       onChange={e => handleNameChange(e.target.value)}
+       />
+
+      <button type="button">
+        Adicionar
+      </button>
+
+      <Card name="Rodrigo" time="10:55:25"/>
+      <Card name="João" time="11:00:10"/>
+      <Card name="Ana" time="12:10:32"/>
     </div>
   )
 }
